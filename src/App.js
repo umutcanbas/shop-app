3c9d6942f590aa3f467d177672786c9d9c631142
Umutcan BAŞ <umutbasinfo@gmail.com>
2022-11-07 15:58:13 +0300

    .

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Navi from "./Navi";
 import ProductList from "./ProductList";
 
 function App() {
-  let titleProduct= "Product List"
-  let titleCategory= "Category List"
+  let productInfo = { title: "ProdutList" };
+  let categoryInfo = { title: "Category List" };
 
   return (
     <div>
@@ -16,13 +16,11 @@ function App() {
         </Row>
         <Row>
           <Col xs="3">
-          <CategoryList title={titleCategory} />
+            <CategoryList info={categoryInfo} />
           </Col>
           <Col xs="9">
-          <ProductList title={titleProduct} />
+            <ProductList info={productInfo} />
           </Col>
-          
-          
         </Row>
       </Container>
     </div>
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,23 +2,32 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    state: {
-    }
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
+    currentCategory: "",
+  };
+
+  changeCategory=(category)=>{
+    this.setState({currentCategory:category.categoryName})
+    
   }
+
   render() {
     return (
       <div>
-        <h3>{this.props.title} </h3>
+        <h3>{this.props.info.title} </h3>
 
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem onClick={()=>this.changeCategory(category)} key={category.categoryId}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
+        <h4>{this.state.currentCategory} </h4>
       </div>
     );
   }
